7ba03491d69940ccbf43dac3dc6b0375beacb304
Alex <martinhaas6@gmail.com>
2021-11-17 22:32:13 -0500

    Adding main.py with some skeleton code

---
+++ main.py
@@ -0,0 +1,33 @@
+
+topRow = ""
+midRow = ""
+botRow = ""
+XMove = 1
+OMove = 3
+spaces = [0]*9
+turn = 0
+
+def isValidMove():
+    print("yo")
+
+def applyMove():
+    if turn == 0:
+
+       turn = 1
+    else:
+
+        turn = 0
+
+def printGrid():
+    i = 0
+    j = 0
+    print(topRow)
+    print(midRow)
+    print(botRow)
+
+def isWin(x):
+    print(x)
+isWin(1)
+
+
+printGrid()
